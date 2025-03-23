f749baf411f18cb65b4bdb530eeeaeeec1c50219
elzorroartico <gilabert.alexander@gmail.com>
2025-03-23 20:35:53 +0100

    removed comments

--- problemtools/verifyproblem.py
+++ problemtools/verifyproblem.py
@@ -1470,9 +1470,7 @@ class OutputValidators(ProblemPart):
 
 class OutputVisualizer(ProblemPart):
     PART_NAME = 'output_visualizer'
-    _visualizer = 'none'
   
-    #TODO fix setup
     def setup(self): #find output_vis
         self._visualizer = run.find_programs(os.path.join(self.problem.probdir,'output_visualizer'), 
         work_dir=self.problem.tmpdir)
@@ -1488,11 +1486,7 @@ class OutputVisualizer(ProblemPart):
             context.submit_background_work(lambda v: v.compile(), self._visualizer) #TODO make sure it works with background_work
             self._has_precompiled = True
         
-    # def _actual_visualizer(self) -> list: #Wrong wrong # inte nödvändig?
-    #     vis = self._visualizer
-    #     if self.problem.get(ProblemConfig)['visualizer'] == 'none': 
-    #         visuals = ['none'] #Change to variable _none_visualizer? 
-    #         return [visuals for vis in visuals if vis is not None]
+
     def create_folder(judge_image_name, submission_name):
         default_path = Path(self.problem.get(__name__) + "/"+ f"{judge_image_name}" + "/"+ f"{submission_name}")
         os.mkdir(default_path)
@@ -1510,27 +1504,7 @@ class OutputVisualizer(ProblemPart):
         
         res = self.visualize(self.problem.get((ProblemTestCases)['root_group'].get_all_testcases()), "s") #TODO get submission output 
 
-        # if self.problem.config(ProblemConfig)['visualizers'] == 'none' and self._default_visualizer is None:
-
-        #BELOW FROM OUTPUTVAL 
-        # fd, file_name = tempfile.mkstemp()
-        # os.close(fd)
-        # for (desc, case) in _JUNK_CASES:
-        #     f = open(file_name, "wb")
-        #     f.write(case)
-        #     f.close()
-        #     rejected = False
-        #     for testcase in self.problem.get(ProblemTestCases)['root_group'].get_all_testcases():
-        #         result = self.visualize(testcase, file_name)
-        #         if result.verdict != 'AC':
-        #             rejected = True
-        #             if result.verdict == 'JE':
-        #                 self.error(f'{desc} as output, and output validator flags "{" ".join(flags)}" gave {result}')
-        #                 break
-        #         if not rejected:
-        #             self.warning(f'{desc} gets AC')
-        #     os.unlink(file_name)
-
+       
 
     #b"89 50 4E 47 0D 0A 1A 0A", file signatures in hex code
     #b"FF D8 FF E0",
@@ -1560,7 +1534,7 @@ class OutputVisualizer(ProblemPart):
             judge_image_name = ""
             submission_name = ""
             self.create_folder(judge_image_name, submission_name)
-            
+
         if self._visualizer().compile()[0]: 
             tempimage = self._visualizer.run(submission_output,
             args =[testcase.infile])
@@ -1568,7 +1542,7 @@ class OutputVisualizer(ProblemPart):
             res.append(self.check_image_type(tempimage))
 
             if save_image:
-                #add to tmpdir
+                #add to dir
                 pass
                 
         return res
